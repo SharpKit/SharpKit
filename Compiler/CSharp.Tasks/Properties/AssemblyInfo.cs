876e5222ee1d92fa1a52d33eef6028bab6f901b1
Sebastian Loncar <arakis@stne.net>
2014-08-20 17:48:20 +0200

    new version 5.4.6-preview

--- Compiler/CSharp.Tasks/Properties/AssemblyInfo.cs
+++ Compiler/CSharp.Tasks/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("v5.4.6-preview")]
+[assembly: AssemblyFileVersion("5.4.6-preview")]

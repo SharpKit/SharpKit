e36959d435ec81d8ef74bfe25c439e7620443774
Sebastian Loncar <arakis@stne.net>
2014-10-30 16:20:14 +0100

    updated nuget generation

--- contrib/nuget/core/tools/install.ps1
+++
@@ -1,7 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-#[Reflection.Assembly]::Load([System.IO.FILE]::ReadAllBytes("C:\Users\sebastian.sharpkit\Documents\Visual Studio 2010\Projects\psSharpKit\bin\Debug\SharpKit.NuGet.dll"))
-#[SharpKit.NuGet.Installer]::Register($project.FullName, $package.Version.Version);
-
-. "$toolsPath/lib.ps1"
-
-register $project $package $toolsPath
--- contrib/nuget/core/tools/uninstall.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-#[Reflection.Assembly]::Load([System.IO.FILE]::ReadAllBytes("C:\Users\sebastian.sharpkit\Documents\Visual Studio 2010\Projects\psSharpKit\bin\Debug\SharpKit.NuGet.dll"))
-#[SharpKit.NuGet.Installer]::Unregister($project.FullName);
-
-. "$toolsPath/lib.ps1"
-unregister $project

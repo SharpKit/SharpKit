576ef62a7cee9ac95df7c0427232d1d5dc0d4dc2
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-08-30 11:58:57 +0200

    use corex netstandard

--- Compiler/skc5/make.cmd
+++ Compiler/skc5/make.cmd
@@ -3,11 +3,11 @@
 
 IF not "%1" == "release" (
 
-%msbuild% skc5.csproj
+dotnet msbuild skc5.csproj
 
 ) ELSE (
 
-%msbuild% skc5.csproj /p:Configuration=Release /p:DebugSymbols=false /p:DebugType=None
+dotnet msbuild skc5.csproj /p:Configuration=Release /p:DebugSymbols=false /p:DebugType=None
 
 )
 
